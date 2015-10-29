ac030dffa70dbfd44187204f57c8b48650b53fca
feligrey <gfisgod2015@163.com>
2015-10-29 09:58:39 +0800

    0.1

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 define(function(require) {
-	var MindMap = require('mindmap');
 
-	var mindmap = new MindMap('mindmap');
-	//mindmap.readData(data);
-	
+	var MindMap = require('mindmap');
+	var mindmap = new MindMap('mindmap');	
 	var button=document.getElementById("button");
+
 	button.style.zIndex=mindmap.zIndex('panel');
 	button.onclick = function() {
 		mindmap.download('data.json');
